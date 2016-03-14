385ec70867852ec3c1ad0cd5d6c77446a168f7e4
Nicolai Czempin <nicolai.czempin@gmail.com>
2016-03-14 01:35:39 +0100

    Add basic scrolling

--- terminal.c
+++ terminal.c
@@ -8,6 +8,7 @@ static size_t terminal_row;
 static size_t terminal_column;
 static uint8_t terminal_color;
 static uint16_t* terminal_buffer;
+const uint32_t VIDEO_CHARACTER_MEMORY_START = 0xB8000;
 
 /* Updates the hardware cursor: the little blinking line
  *  on the screen under the last character pressed! */
@@ -41,7 +42,7 @@ void terminal_initialize()
 	terminal_column = 0;
 	move_cursor();
 	terminal_color = vga_make_color(COLOR_GREEN, COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
+	terminal_buffer = (uint16_t*) VIDEO_CHARACTER_MEMORY_START;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
@@ -61,14 +62,22 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_make_color_entry(c, color);
 }
-
+void scroll(){
+	for (size_t i = 0; i < (VGA_HEIGHT - 1) * VGA_WIDTH; ++i){
+		terminal_buffer[i] = terminal_buffer[i + VGA_WIDTH];
+	}
+	for (size_t i = (VGA_HEIGHT - 1) * VGA_WIDTH; i < VGA_HEIGHT * VGA_WIDTH; ++i){
+		terminal_buffer[i] = vga_make_color_entry(' ', terminal_color);
+	}
+}
 void terminal_putchar(char c)
 {
 	switch(c){
 		case '\n':
 			terminal_column = 0;
 			if (++terminal_row >= VGA_HEIGHT) {
-				terminal_row = 0;
+				scroll();
+				terminal_row=VGA_HEIGHT-1;;
 			}
 			break;
 		case '\b':
@@ -83,7 +92,7 @@ void terminal_putchar(char c)
 			if (++terminal_column >= VGA_WIDTH) {
 				terminal_column = 0;
 				if (++terminal_row >= VGA_HEIGHT) {
-					terminal_row = 0;
+					scroll();
 				}
 			}
 	}
